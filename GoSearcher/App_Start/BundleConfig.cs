e04e99abd6ebfa7171f9fe404afe73787b939703
Brian Engel <brianengel@gmail.com>
2014-03-02 02:31:56 -0500

    Fixed bundling issue

--- GoSearcher/App_Start/BundleConfig.cs
+++ GoSearcher/App_Start/BundleConfig.cs
@@ -8,16 +8,12 @@ namespace GoSearcher
 {
     public class BundleConfig
     {
-        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/site").Include(
+            bundles.Add(new StyleBundle("~/bundles/site").Include(
                         "~/css/normalize.css",
                         "~/css/bootstrap.css",
                         "~/css/site.css"));
-
         }
     }
 }
